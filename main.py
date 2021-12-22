2a6213d54cb8ae6152856c21073ecc167e7aa9f8
MeowNya <Evina666@gmail.com>
2021-12-23 03:34:26 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'MeowNya'
+
+
+import datetime as DT
+import time
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+
+url = 'https://goldapple.ru/brands/elian-russia'
+prev_val = None
+
+while True:
+    rs = requests.get(url)
+
+    soup = BeautifulSoup(rs.content, "html.parser")
+
+    need = soup.select_one("#toolbar-amount > .toolbar-number").text
+    print(need, " товаров ", DT.datetime.now().strftime("%H:%M %d.%m.%Y"), sep="")
+    print(f"{need} товаров {DT.datetime.now():%H:%M %d.%m.%Y}")
+
+    if prev_val is not None and prev_val != need:
+        prev_val = need
+        print(f"Alarm! Warning! Products = {need} now!", file=sys.stderr)
+
+    time.sleep(60*60)
